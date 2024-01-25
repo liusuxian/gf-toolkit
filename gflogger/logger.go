e8a01a4930ada49b2c3a63ffc52223f1b44c9136
liusuxian <382185882@qq.com>
2024-01-25 21:05:33 +0800

    修改中间件的日志打印堆栈层数

--- gflogger/logger.go
+++ gflogger/logger.go
@@ -2,7 +2,7 @@
  * @Author: liusuxian 382185882@qq.com
  * @Date: 2024-01-19 20:59:43
  * @LastEditors: liusuxian 382185882@qq.com
- * @LastEditTime: 2024-01-22 22:13:17
+ * @LastEditTime: 2024-01-25 21:01:37
  * @Description:
  *
  * Copyright (c) 2024 by liusuxian email: 382185882@qq.com, All Rights Reserved.
@@ -16,80 +16,86 @@ import (
 )
 
 // Log 获取log对象
-func Log(name ...string) *glog.Logger {
+func Log(skip int, name ...string) *glog.Logger {
 	if len(name) > 0 && name[0] != "" {
-		return g.Log(name[0]).Skip(1).Line()
+		return g.Log(name[0]).Skip(skip).Line()
 	}
-	return g.Log().Skip(1).Line()
+	return g.Log().Skip(skip).Line()
 }
+
 func Print(ctx context.Context, v ...any) {
-	Log("access").Print(ctx, v...)
+	defaultLog("access").Print(ctx, v...)
 }
 
 func Printf(ctx context.Context, format string, v ...any) {
-	Log("access").Printf(ctx, format, v...)
+	defaultLog("access").Printf(ctx, format, v...)
 }
 
 func Info(ctx context.Context, v ...any) {
-	Log("access").Info(ctx, v...)
+	defaultLog("access").Info(ctx, v...)
 }
 
 func Infof(ctx context.Context, format string, v ...any) {
-	Log("access").Infof(ctx, format, v...)
+	defaultLog("access").Infof(ctx, format, v...)
 }
 
 func Debug(ctx context.Context, v ...any) {
-	Log("access").Debug(ctx, v...)
+	defaultLog("access").Debug(ctx, v...)
 }
 
 func Debugf(ctx context.Context, format string, v ...any) {
-	Log("access").Debugf(ctx, format, v...)
+	defaultLog("access").Debugf(ctx, format, v...)
 }
 
 func Notice(ctx context.Context, v ...any) {
-	Log("access").Notice(ctx, v...)
+	defaultLog("access").Notice(ctx, v...)
 }
 
 func Noticef(ctx context.Context, format string, v ...any) {
-	Log("access").Noticef(ctx, format, v...)
+	defaultLog("access").Noticef(ctx, format, v...)
 }
 
 func Warning(ctx context.Context, v ...any) {
-	Log("error").Warning(ctx, v...)
+	defaultLog("error").Warning(ctx, v...)
 }
 
 func Warningf(ctx context.Context, format string, v ...any) {
-	Log("error").Warningf(ctx, format, v...)
+	defaultLog("error").Warningf(ctx, format, v...)
 }
 
 func Error(ctx context.Context, v ...any) {
-	Log("error").Error(ctx, v...)
+	defaultLog("error").Error(ctx, v...)
 }
 
 func Errorf(ctx context.Context, format string, v ...any) {
-	Log("error").Errorf(ctx, format, v...)
+	defaultLog("error").Errorf(ctx, format, v...)
 }
 
 func Fatal(ctx context.Context, v ...any) {
-	Log("error").Fatal(ctx, v...)
+	defaultLog("error").Fatal(ctx, v...)
 }
 
 func Fatalf(ctx context.Context, format string, v ...any) {
-	Log("error").Fatalf(ctx, format, v...)
+	defaultLog("error").Fatalf(ctx, format, v...)
 }
 
 func Critical(ctx context.Context, v ...any) {
-	Log("error").Critical(ctx, v...)
+	defaultLog("error").Critical(ctx, v...)
 }
 
 func Criticalf(ctx context.Context, format string, v ...any) {
-	Log("error").Criticalf(ctx, format, v...)
+	defaultLog("error").Criticalf(ctx, format, v...)
 }
 
 func Panic(ctx context.Context, v ...any) {
-	Log("error").Panic(ctx, v...)
+	defaultLog("error").Panic(ctx, v...)
 }
 
 func Panicf(ctx context.Context, format string, v ...any) {
-	Log("error").Panicf(ctx, format, v...)
+	defaultLog("error").Panicf(ctx, format, v...)
+}
+
+// defaultLog 默认log对象
+func defaultLog(name ...string) *glog.Logger {
+	return Log(1, name...)
 }
